7165d0555432b29208e0c2e8066329b69e85c533
alejotrenti <aletrenti@outlook.com>
2024-10-14 15:28:41 -0300

    fix

--- src/app/blog/[uid]/page.tsx
+++ src/app/blog/[uid]/page.tsx
@@ -1,9 +1,10 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
-import { components } from "@/slices";
+
+import ContentBody from "@/components/ContentBody";
+import { formatDate } from "@/utils/formatDate";
 
 type Params = { uid: string };
 
@@ -13,7 +14,7 @@ export default async function Page({ params }: { params: Params }) {
     .getByUID("blog_post", params.uid)
     .catch(() => notFound());
 
-  return <SliceZone slices={page.data.slices} components={components} />;
+  return <ContentBody page={page} />;
 }
 
 export async function generateMetadata({
@@ -27,7 +28,7 @@ export async function generateMetadata({
     .catch(() => notFound());
 
   return {
-    title: page.data.meta_title,
+    title: page.data.title,
     description: page.data.meta_description,
   };
 }
